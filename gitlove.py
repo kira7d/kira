0b5890e884e4f05b76cb5647674804d3303ff5f4
kira7d <dovbushkira07@gmail.com>
2022-10-09 19:10:23 +0300

    ha

--- gitlove.py
+++ gitlove.py
@@ -1,8 +1,15 @@
 # a = [1, 34, 66, 83, 0]
 # print(a[-3::])
 
-a = input().lower()
-if a[::] == a[::-1]:
-    print("yes")
-else:
-    print("no")
+a = [1, 2, 3, 4, 5, 6]
+c = 1
+for b in a:
+    c *= 3
+    print(c)
+
+a = [1, 2, 3, 4, 5, 6]
+c = 1
+for b in a:
+    c *= b
+    print(c)
+
