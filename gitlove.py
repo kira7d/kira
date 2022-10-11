e1d681743788b019f09cbc14c9d3e8dc29bd2d56
kira7d <dovbushkira07@gmail.com>
2022-10-11 16:59:54 +0300

    hi

--- gitlove.py
+++ gitlove.py
@@ -1,15 +1,52 @@
 # a = [1, 34, 66, 83, 0]
 # print(a[-3::])
 
-a = [1, 2, 3, 4, 5, 6]
-c = 1
-for b in a:
-    c *= 3
-    print(c)
+# a = [1, 2, 3, 4, 5, 6]
+# c = 1
+# for b in a:
+#     c *= 3
+#     print(c)
+#
+# a = [1, 2, 3, 4, 5, 6]
+# c = 1
+# for b in a:
+#     c *= b
+#     print(c)
 
-a = [1, 2, 3, 4, 5, 6]
-c = 1
-for b in a:
-    c *= b
-    print(c)
+# for i in range (1,11):
+#     print(i)
+#
+# for i in range(11):
+#     print(i,end = " ")
+
+# for i in range(10,-1, -1):
+#     print(i,end = " ")
+
+# for i in range(-10,-1, 2):
+#     print(i, end = " ")
+
+# for i in range(1, 22, 3):
+#     print(i, end = " ")
+
+# a = map(int, input().split())
+# b = 0
+# for i in a:
+#     if i % 2 != 0:
+#         b += i
+# print(b)
+
+# a = list(map(str,input().split()))
+# for i in range(len(a)):
+#     a[i] = len(a[i])
+# print(*a)
+
+# a = 0
+# for i in range(12):
+#     if i % 2 == 0:
+#         a += i
+# print(a)
+
+
+
+
 
